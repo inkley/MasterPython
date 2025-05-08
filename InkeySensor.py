d3b3e6b3dee2f3525121b24d8df195277840c9df
unknown <gletchm@gmail.com>
2025-05-08 11:22:19 -0600

    Got a command line, capture CAN data and create a CSV file.

--- InkeySensor.py
+++ InkeySensor.py
@@ -1,24 +1,123 @@
 import can
+import csv
+import datetime
+import struct
+import cmd
+import threading
 
-# Candlelight firmware on Linux
-#bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
+class CANBusCommander(cmd.Cmd):
+    intro = "Inkley Sensor Command Line Interface. Type 'help' for commands.\n"
+    prompt = "> "
 
-# Stock slcan firmware on Linux
-# bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=500000)
-def send_one():
+    def __init__(self):
+        super().__init__()
+        self.bus = None
+        self.streaming = False
+        self.stream_thread = None
+        self.csv_file = 'Inkley_sensor_data.csv'
+        self.sensor_data = {
+            'Pressure1': '',
+            'Pressure2': '',
+            'Temperature1': '',
+            'Temperature2': ''
+        }
+        # Define CAN ID and sensor IDs
+        self.CAN_ID = 0x7DF
+        self.SENSOR_IDS = {
+            0x01: 'Pressure1',
+            0x02: 'Pressure2',
+            0x03: 'Temperature1',
+            0x04: 'Temperature2'
+        }
+        self.version = "1.0.0"
 
-    bus = can.interface.Bus(interface='slcan', channel='COM8', bitrate=1000000)
+    def parse_sensor_value(self, data):
+        if len(data) >= 6 and data[0] == 5:  # Check length byte
+            raw_value = data[2:6]
+            return struct.unpack('>i', bytes(raw_value))[0]
+        return None
 
-    msg = can.Message(arbitration_id=0x107,
-                    data=[0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], #Read Sensor Module Version.
-                    is_extended_id=False)
+    def stream_data(self):
+        # Initialize CAN bus interface for CANable
+        # * don't forget to choose the right COM port.
+        #Mac
+        #bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyUSB0', bitrate=1000000)
 
-    try:
-        bus.send(msg)
-        print("Message sent on {}".format(bus.channel_info))
-    except can.CanError:
-        print("Message NOT sent")
+        #Windows
+        bus = can.interface.Bus(interface='slcan', channel='COM8', bitrate=1000000)
+        
+        with open(self.csv_file, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Timestamp', 'Pressure1', 'Pressure2', 'Temperature1', 'Temperature2'])
+            last_write_time = datetime.datetime.now()
 
+            try:
+                while self.streaming:
+                    for msg in self.bus:
+                        if not self.streaming:
+                            break
+                        current_time = datetime.datetime.now()
+                        
+                        if msg.arbitration_id == self.CAN_ID and len(msg.data) == 8:
+                            sensor_id = msg.data[1]
+                            if sensor_id in self.SENSOR_IDS:
+                                value = self.parse_sensor_value(msg.data)
+                                if value is not None:
+                                    self.sensor_data[self.SENSOR_IDS[sensor_id]] = str(value)
 
-if __name__ == "__main__":
-    send_one()
+                        if (current_time - last_write_time).total_seconds() >= 1:
+                            writer.writerow([
+                                current_time.isoformat(),
+                                self.sensor_data['Pressure1'],
+                                self.sensor_data['Pressure2'],
+                                self.sensor_data['Temperature1'],
+                                self.sensor_data['Temperature2']
+                            ])
+                            last_write_time = current_time
+
+            except Exception as e:
+                print(f"Streaming error: {e}")
+            finally:
+                self.bus.shutdown()
+                self.bus = None
+                print(f"CAN sensor data saved to {self.csv_file}")
+
+    def do_version(self, arg):
+        """Display the version of the CAN bus interface"""
+        print(f"Version: {self.version}")
+
+    def do_start(self, arg):
+        """Start real-time CAN bus streaming"""
+        if not self.streaming:
+            self.streaming = True
+            self.stream_thread = threading.Thread(target=self.stream_data)
+            self.stream_thread.start()
+            print("Started real-time streaming")
+        else:
+            print("Streaming is already active")
+
+    def do_stop(self, arg):
+        """Stop real-time CAN bus streaming"""
+        if self.streaming:
+            self.streaming = False
+            if self.stream_thread:
+                self.stream_thread.join()
+            print("Stopped real-time streaming")
+        else:
+            print("Streaming is not active")
+
+    def do_quit(self, arg):
+        """Exit the command line interface"""
+        if self.streaming:
+            self.streaming = False
+            if self.stream_thread:
+                self.stream_thread.join()
+        print("Exiting Sensor Commander")
+        return True
+
+    def do_exit(self, arg):
+        """Alias for quit command"""
+        return self.do_quit(arg)
+
+if __name__ == '__main__':
+    CANBusCommander().cmdloop()
